c410372a11f10eb049f7f36432f04d9665d1dd8e
sabbirHossain <sabbirshnt@gmail.com>
2026-06-20 14:36:20 +0600

    done

--- src/static/latest-event.js
+++ src/static/latest-event.js
@@ -4,4 +4,9 @@ export const latestEvent = [
     pathname: "/event-details/4/33439203",
     show: true,
   },
+  {
+    eventName: "FIFA 2026",
+    pathname: "/event-details/4/1780591416",
+    show: true,
+  },
 ];
