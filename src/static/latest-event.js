9255d7bf5d8f24ba145414b322297a83b440ca77
sabbirHossain <sabbirshnt@gmail.com>
2026-07-06 14:28:52 +0600

    done

--- src/static/latest-event.js
+++ src/static/latest-event.js
@@ -1,10 +1,5 @@
 export const latestEvent = [
   {
-    eventName: "Women's T20 WC",
-    pathname: "/event-details/4/33439203",
-    show: true,
-  },
-  {
     eventName: "FIFA 2026",
     pathname: "/event-details/4/1780591416",
     show: true,
