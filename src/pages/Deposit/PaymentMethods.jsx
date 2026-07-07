62647f1a4fe1282746c949ce8dca57c9248bf2c9
sabbirHossain <sabbirshnt@gmail.com>
2026-07-07 14:14:20 +0600

    done

--- src/pages/Deposit/PaymentMethods.jsx
+++ src/pages/Deposit/PaymentMethods.jsx
@@ -118,7 +118,9 @@ const PaymentMethods = ({
                       {method?.type == "bank" && (
                         <CiBank size={20} color="gray" />
                       )}
-                      {method?.type == "upi" || method?.type == "pg" ? (
+                      {method?.type == "upi" ||
+                      method?.type == "pg" ||
+                      method?.gateway === 1 ? (
                         <img
                           style={{ height: "20px", width: "20px" }}
                           src={images.upi}
