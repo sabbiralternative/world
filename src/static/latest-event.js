b37109a072865cac8b7376d5442dae7b094315ff
sabbirHossain <sabbirshnt@gmail.com>
2026-07-20 16:38:42 +0600

    done

--- src/static/latest-event.js
+++ src/static/latest-event.js
@@ -1,7 +1,7 @@
 export const latestEvent = [
-  {
-    eventName: "FIFA 2026",
-    pathname: "/event-details/4/1780591416",
-    show: true,
-  },
+  // {
+  //   eventName: "FIFA 2026",
+  //   pathname: "/event-details/4/1780591416",
+  //   show: true,
+  // },
 ];
